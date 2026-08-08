ff643e5d0aea64b83febf026f9c7145a20788f3a
Ashwin <satviashwin369@gmail.com>
2026-08-08 16:51:28 +0530

    Time: 44 ms (74.81%), Space: 29.2 MB (47.22%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -11,15 +11,23 @@ public:
                 int sum = nums[i] + nums[left] + nums[right];
                 if (sum == 0) {
                     result.push_back({nums[i], nums[left], nums[right]});
-                    while (left < right && nums[left] == nums[left + 1]) {
+
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) {
                         left++;
                     }
-                    while (left < right && nums[right] == nums[right - 1]) {
+                    while (left < right && nums[right] == nums[right + 1]) {
                         right--;
                     }
                 }
-                if (sum < 0) left++;
-                else right--;
+                else if (sum < 0) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
             }
         }
         return result;
